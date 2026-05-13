5f6aa7463d41e00eaff1bd33b7a9656e3ef20031
YXUNGGG <s.krivostanenko@mail.ru>
2026-05-13 23:46:12 +0500

    security changes

--- auth.config.ts
+++ auth.config.ts
@@ -2,10 +2,6 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const authConfig = {
-  pages: {
-    signIn: "/login"
-  },
-
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
